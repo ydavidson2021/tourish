574ebba58d0d6370dd0f956f1a5a8c03082b2a40
Yenny D <ydavidson2021@gmail.com>
2020-10-04 19:51:46 -0500

    Functional Components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,41 +1,34 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import SiteInfo from './SiteInfoComponent';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedSite : null
-        };
-    }
+function RenderDirectoryItem({site, onClick}) {
+    return (
+        <Card onClick={() => onClick(site.id)}>
+            <CardImg width="400" height="200"  src={site.image} alt={site.name} />
+            <CardImgOverlay>
+                <CardTitle>{site.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
-    onSiteSelect(site) {
-        this.setState({selectedSite: site});
-    }
-    
-    render() {
-        const directory = this.props.sites.map(site => {
-            return (
-                <div key={site.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onCLick(site.id)}>
-                        <CardImg width="400" height="200"  src={site.image} alt={site.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{site.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
+function Directory(props) {
 
+    const directory = props.sites.map(site => {
         return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
+            <div key={site.id} className="col-md-5 m-1">
+                <RenderDirectoryItem site={site} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
 }
 
 export default Directory;
